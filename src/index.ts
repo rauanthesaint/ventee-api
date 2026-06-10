9dcdc55acacfe5b5562a931f635c41ddb9977cb1
Rauan Karanov <rauanthesaint@proton.me>
2026-06-10 16:13:51 +0500

    chore: added cors plugin

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
+
 import { env, loggerOptions } from "./config.js";
 import { Bot, webhookCallback } from "grammy";
 
 const app = Fastify({
     logger: env.NODE_ENV === "development" ? loggerOptions : true,
+});
+
+app.register(cors, {
+    origin: true,
+    credentials: true,
 });
 
 const bot = new Bot(env.BOT_TOKEN);
@@ -25,11 +32,12 @@ app.register(
     { prefix: "/v1" },
 );
 
+await app.ready();
 await app.listen({ port: env.PORT, host: "0.0.0.0" });
 
 if (env.NODE_ENV === "production") {
     await bot.api.setWebhook(`${env.HOSTNAME}/v1/webhook`);
 } else {
     await bot.api.deleteWebhook();
-    await bot.start();
+    bot.start();
 }
