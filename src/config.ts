82c09cfe0b6563a6a17bb13ed84ac9e4c8dfc002
Rauan Karanov <rauanthesaint@proton.me>
2026-06-10 14:53:24 +0500

    chore: test deploying on render.com

---
+++ src/config.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import type { FastifyLoggerOptions } from "fastify";
+import type { PinoLoggerOptions } from "fastify/types/logger";
+import z from "zod";
+
+const schema = z.object({
+    PORT: z.int().min(1000).max(9999).default(5000),
+    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+    HOSTNAME: z.url(),
+    BOT_TOKEN: z.string().min(1),
+});
+
+export const env = schema.parse(process.env);
+
+export const loggerOptions: FastifyLoggerOptions & PinoLoggerOptions = {
+    level: "info",
+    transport: {
+        target: "pino-pretty",
+        options: {
+            translateTime: "SYS:standard",
+            ignore: "pid,hostname",
+        },
+    },
+};
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import Fastify from "fastify";
+import { env, loggerOptions } from "@/config";
+import { Bot, webhookCallback } from "grammy";
+
+const application = Fastify({
+    logger: env.NODE_ENV === "development" ? loggerOptions : true,
+});
+
+// add cors plugin
+
+const bot = new Bot(env.BOT_TOKEN);
+
+bot.command("start", async (ctx) => {
+    await ctx.reply("Hello!");
+});
+
+application.register(
+    (instance) =>
+        instance.get("/", async (req, rep) => {
+            return rep.status(200).send(req.headers);
+        }),
+    { prefix: "v1" },
+);
+
+if (env.NODE_ENV === "development") {
+    await bot.api.deleteWebhook();
+    await bot.start();
+} else {
+    application.post("/webhook", webhookCallback(bot, "fastify"));
+    await application.listen({ port: env.PORT });
+    await bot.api.setWebhook(`${env.HOSTNAME}/webhook`);
+}
